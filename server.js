f1f8266e2dadfd4b6b7638ff7081b732052a65ed
Philip Lipman <philip.lipman@gmail.com>
2017-08-01 21:06:25 -0700

    auth refactor

--- server.js
+++ server.js
@@ -4,86 +4,76 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const mongodb = require('mongodb');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
-const ObjectId = mongodb.ObjectId;
+const config = require('./config');
+const User = require('./app/models/user');
 
 const CONTACTS_COLLECTION = 'contacts';
+const port = process.env.PORT || 8080;
 
 const app = express();
 
-const allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-        res.send(200);
-    }
-    else {
-        next();
-    }
-};
-
-app.use(allowCrossDomain);
-app.use(express.static(__dirname + '/public'));
+// const allowCrossDomain = function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+//
+//     // intercept OPTIONS method
+//     if ('OPTIONS' == req.method) {
+//         res.send(200);
+//     }
+//     else {
+//         next();
+//     }
+// };
+//
+// app.use(allowCrossDomain);
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// reuse connection pool
-let db;
-
-// connect to database before starting server
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    }
-
-    // save database object from callback for reuse
-    db = database;
-
-    const server = app.listen(process.env.PORT || 8080, function () {
-        const port = server.address().port;
-        console.log('App now running on port: ', port);
-    });
-});
+mongoose.connect(config.database);
+app.set('superSecret', config.secret);
 
 function handleError(res, reason, message, code) {
     console.log('ERROR: ' + reason);
     res.status(code || 500).json({ 'error': message });
 }
 
-// '/contacts'
-//     GET: finds all contacts
-//     POST: creates a new contact
-
-app.get('/contacts', function(req, res) {
-    db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
-        if (err) {
-            handleError(res, err.message, 'Failed to get contacts.');
-        } else {
-            res.status(200).json(docs);
-        }
-    });
+app.get('/', function(req, res) {
+    res.send('Hello! The API is running...');
 });
 
-app.post('/contacts', function(req, res) {
-    const newContact = req.body;
-    newContact.createDate = new Date();
+app.listen(port);
+console.log('Magic happens at port:' + port);
 
-    if (!(req.body.firstName || req.body.lastName)) {
-        handleError(res, 'Invalid user input', 'Must provide a first or last name.', 400);
-    }
-
-    db.collection(CONTACTS_COLLECTION).insertOne(newContact, function (err, doc) {
-        if (err) {
-            handleError(res, err.message, 'Failed to create new contact.');
-        } else {
-            res.status(201).json(doc.ops[0]);
-        }
-    });
-});
+// app.get('/contacts', function(req, res) {
+//     db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+//         if (err) {
+//             handleError(res, err.message, 'Failed to get contacts.');
+//         } else {
+//             res.status(200).json(docs);
+//         }
+//     });
+// });
+//
+// app.post('/contacts', function(req, res) {
+//     const newContact = req.body;
+//     newContact.createDate = new Date();
+//
+//     if (!(req.body.firstName || req.body.lastName)) {
+//         handleError(res, 'Invalid user input', 'Must provide a first or last name.', 400);
+//     }
+//
+//     db.collection(CONTACTS_COLLECTION).insertOne(newContact, function (err, doc) {
+//         if (err) {
+//             handleError(res, err.message, 'Failed to create new contact.');
+//         } else {
+//             res.status(201).json(doc.ops[0]);
+//         }
+//     });
+// });
 
 // '/contacts/:id'
 //     GET: find contact by id
