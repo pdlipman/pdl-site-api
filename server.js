31ec5930e3c1eeb84032f6333bbf0acd6705bc41
Philip Lipman <philip.lipman@gmail.com>
2017-08-01 20:11:26 -0700

    contacts GET endpoint

--- server.js
+++ server.js
@@ -43,7 +43,13 @@ function handleError(res, reason, message, code) {
 //     POST: creates a new contact
 
 app.get('/contacts', function(req, res) {
-
+    db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+        if (err) {
+            handleError(res, err.message, 'Failed to get contacts.');
+        } else {
+            res.status(200).json(docs);
+        }
+    });
 });
 
 app.post('/contacts', function(req, res) {
