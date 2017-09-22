737ade754b84c4ec21c1612d8cc5bcee775579b9
Philip Lipman <philip.lipman@gmail.com>
2017-09-21 22:32:56 -0700

    fixed typo

--- app/models/user.js
+++ app/models/user.js
@@ -38,9 +38,8 @@ UserSchema.pre('save', function(next) {
     if (!user.isModified('password')) {
         return next();
     }
-    bcrypt.ge
 
-    bcrypt.genSalt(saltFactor, function(err, salt) {
+    bcrypt.getSalt(saltFactor, function(err, salt) {
         if (err) {
             return next(err);
         }
